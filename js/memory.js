e49686f655995d381bc7409178c8745108738051
u1986966 <160513211+u1986966@users.noreply.github.com>
2024-03-18 16:31:03 +0100

    Commit_Brunch
    Practica3

--- js/memory.js
+++ js/memory.js
@@ -4,71 +4,72 @@ export var game = function(){
     const card = {
         current: back,
         clickable: true,
-        visible: false, // per no ser clicada quan es veu
+        waiting: false,
+        isDone: false,
         goBack: function (){
             setTimeout(() => {
                 this.current = back;
                 this.clickable = true;
-                this.visible = false;
                 this.callback();
             }, 1000);
         },
-        goFront: function (){
+        goFront: function (last){
+            if (last)
+                this.waiting = last.waiting = false;
+            else
+                this.waiting = true;
             this.current = this.front;
             this.clickable = false;
-            this.visible = true;
             this.callback();
+        },
+        check: function (other){
+            if (this.front === other.front)
+                this.isDone = other.isDone = true;
+            return this.isDone;
         }
     };
 
-    const default_options = {
-        pairs:2,
-        difficulty:'normal'
-    };
-    var options = JSON.parse(localStorage.options||JSON.stringify(default_options));
     var lastCard;
-    var pairs = options.pairs;
-    var difficulty = options.difficulty;
+    var pairs = 2;
     var points = 100;
-    var temps = 1000;
-    if (options.difficulty == 'hard'){
-        temps = 500;
+    var cards = []; // Llistat de cartes
+
+    var mix = function(){
+        var items = resources.slice(); // Copiem l'array
+        items.sort(() => Math.random() - 0.5); // Aleatòria
+        items = items.slice(0, pairs); // Agafem els primers
+        items = items.concat(items);
+        return items.sort(() => Math.random() - 0.5); // Aleatòria
     }
-    else if (options.difficulty == 'normal'){
-        temps = 1000;
-    }
-    else {
-        temps = 5000;
-    };
     return {
         init: function (call){
-            var items = resources.slice(); // Copiem l'array
-            items.sort(() => Math.random() - 0.5); // Aleatòria
-            items = items.slice(0, pairs); // Agafem els primers
-            items = items.concat(items);
-            items.sort(() => Math.random() - 0.5); // Aleatòria
-
-            var c = items.map(item => Object.create(card, {front: {value:item}, callback: {value:call}}));
-            c.forEach( obj =>{
-                obj.current = obj.front;
-                obj.clickable = false; //inicialment les cartes no es poden clicar
-                obj.visible = false; // tampoc son visibles
-                setTimeout(() => {
-                    obj.current = back;
-                    obj.clickable = true;
-                    obj.callback()
-                }, temps);
+            if (sessionStorage.save){ // Load game
+                let partida = JSON.parse(sessionStorage.save);
+                pairs = partida.pairs;
+                points = partida.points;
+                partida.cards.map(item=>{
+                    let it = Object.create(card);
+                    it.front = item.front;
+                    it.current = item.current;
+                    it.isDone = item.isDone;
+                    it.waiting = item.waiting;
+                    it.callback = call;
+                    cards.push(it);
+                    if (it.current != back && !it.waiting && !it.isDone) it.goBack();
+                    else if (it.waiting) lastCard = it;
+                });
+                return cards;
+            }
+            else return mix().map(item => { // New game
+                cards.push(Object.create(card, { front: {value:item}, callback: {value:call}}));
+                return cards[cards.length-1];
             });
-            
-            return c;
-
-
         },
         click: function (card){
-            if (!card.clickable || card.visible) return;
-            card.goFront();
+            if (!card.clickable) return;
+            card.goFront(lastCard);
             if (lastCard){ // Segona carta
-                if (card.front === lastCard.front){
+                if (card.check(lastCard)){
                     pairs--;
                     if (pairs <= 0){
                         alert("Has guanyat amb " + points + " punts!");
@@ -86,6 +87,42 @@ export var game = function(){
                 lastCard = null;
             }
             else lastCard = card; // Primera carta
+        },
+        save: function (){
+            var partida = {
+                uuid: localStorage.uuid,
+                pairs: pairs,
+                points: points,
+                cards: []
+            };
+            cards.forEach(c=>{
+                partida.cards.push({
+                    current: c.current,
+                    front: c.front,
+                    isDone: c.isDone,
+                    waiting: c.waiting
+                });
+            });
+
+            let json_partida = JSON.stringify(partida);
+
+            fetch("../php/save.php",{
+                method: "POST",
+                body: json_partida,
+                headers: {"content-type":"application/json; charset=UTF-8"}
+            })
+            .then(response=>response.json())
+            .then(json => {
+                console.log(json);
+            })
+            .catch(err=>{
+                console.log(err);
+                localStorage.save = json_partida;
+                console.log(localStorage.save);
+            })
+            .finally(()=>{
+                window.location.replace("../");
+            });
         }
     }
 }();
