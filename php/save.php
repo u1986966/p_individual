f7429f6f21c73b29ae3738511918f92712c21a79
u1986966 <160513211+u1986966@users.noreply.github.com>
2024-03-18 16:50:14 +0100

    canvi save

--- php/save.php
+++ php/save.php
@@ -10,7 +10,7 @@
 
     $encodeCards = json_encode($_SESSION['cards']);
 
-    $conn = oci_connect('u4000775', '*********', 'ORCLCDB');
+    $conn = oci_connect('u1986966', 'muoymccq', 'ORCLCDB');
     $insert="INSERT INTO memory_save
     (uuid, pairs, points, cards )
     VALUES
